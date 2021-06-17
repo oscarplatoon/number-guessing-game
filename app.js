e3cff94ab82fdcda594d55a4421cb7adab4027f5
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-17 07:49:34 -0400

    Basic logic working. Need to refine css

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ console.log("HELLO OSCAR PLATOON!")
 randomNumber = Math.floor(Math.random()*100 + 1)
 console.log(randomNumber)
 
-gameSolved = false
-
+const lowGuess = "Your guess was lower than the target number."
+const highGuess = "Your guess was higher than the target number"
+const correct = "Your guess was correct, You Win!"
 const guess = () => {
-  userInput = document.getElementById('guess-input')
-  console.log(userInput.value)
+  const userInput = document.getElementById('guess-input')
+  const lineBreak = document.createElement('br');
+  const userGuessDiv = document.createElement('div');
+  userGuessDiv.setAttribute('id', 'guess-log')
+  const userGuessText = document.createTextNode(`Your guess was ${userInput.value}`)
+  userGuessDiv.appendChild(userGuessText)
+  let anchorDiv = document.getElementById('game-input')
+  document.body.insertBefore(userGuessDiv, anchorDiv); 
+  let gameAnswer = ""
   if(userInput.value > randomNumber) {
-
+    userGuessDiv.appendChild(lineBreak)
+    gameAnswer = highGuess
+    gameAnswerChild = document.createTextNode(gameAnswer)
+    userGuessDiv.appendChild(gameAnswerChild)
   } else if(userInput.value < randomNumber) {
-
+      userGuessDiv.appendChild(lineBreak)
+      gameAnswer = lowGuess
+      gameAnswerChild = document.createTextNode(gameAnswer)
+      userGuessDiv.appendChild(gameAnswerChild)
   } else if(userInput.value == randomNumber) {
-    gameSolved = true;
-  }
-
-  if (gameSolved == true) {
-    //set html on page to reflect the game is over.
+        userGuessDiv.appendChild(lineBreak)
+        gameAnswer = correct
+        gameAnswerChild = document.createTextNode(gameAnswer)
+        userGuessDiv.appendChild(gameAnswerChild)
   }
 }
