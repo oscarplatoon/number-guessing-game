a095d5032988349979a6977beb3d804c8483093b
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-06-17 11:19:57 -0400

    Cleaned up code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("HELLO OSCAR PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+
 let randomNumber = 0;
 
 const pickRandomNumber = () => {
@@ -8,7 +8,6 @@ const pickRandomNumber = () => {
 }
 
 pickRandomNumber();
-console.log(randomNumber);
 
 const lowGuess = "Your guess was lower than the target number."
 const highGuess = "Your guess was higher than the target number"
@@ -42,33 +41,4 @@ const addGuessToList = (number) => {
   newElem.appendChild(numText)
   const numList = document.getElementById('guess-list')
   numList.appendChild(newElem)
-}
-const guess = () => {
-  const userInput = document.getElementById('guess-input')
-  console.log(userInput)
-  const lineBreak = document.createElement('br');
-  const userGuessDiv = document.createElement('div');
-  userGuessDiv.setAttribute('id', 'guess-log')
-  const userGuessText = document.createTextNode(`Your guess was ${userInput.value}`)
-  userGuessDiv.appendChild(userGuessText)
-  let anchorDiv = document.getElementById('game-input')
-  document.body.insertBefore(userGuessDiv, anchorDiv); 
-  let gameAnswer = ""
-  if(userInput.value > randomNumber) {
-    userGuessDiv.appendChild(lineBreak)
-    gameAnswer = highGuess
-    gameAnswerChild = document.createTextNode(gameAnswer)
-    userGuessDiv.appendChild(gameAnswerChild)
-  } else if(userInput.value < randomNumber) {
-      userGuessDiv.appendChild(lineBreak)
-      gameAnswer = lowGuess
-      gameAnswerChild = document.createTextNode(gameAnswer)
-      userGuessDiv.appendChild(gameAnswerChild)
-  } else if(userInput.value == randomNumber) {
-      userGuessDiv.appendChild(lineBreak)
-      gameAnswer = correct
-      gameAnswerChild = document.createTextNode(gameAnswer)
-      userGuessDiv.appendChild(gameAnswerChild)
-  }
-
 }
