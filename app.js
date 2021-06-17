e5f03a4ae9cc6c3575e90c70a0f159c6cbb1faee
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-06-17 11:19:20 -0400

    Implemented Tom's solution during lecture

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 console.log("HELLO OSCAR PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+let randomNumber = 0;
 
-randomNumber = Math.floor(Math.random()*100 + 1)
-console.log(randomNumber)
+const pickRandomNumber = () => {
+  randomNumber = Math.ceil(Math.random() * 100);
+  return randomNumber;
+}
+
+pickRandomNumber();
+console.log(randomNumber);
 
 const lowGuess = "Your guess was lower than the target number."
 const highGuess = "Your guess was higher than the target number"
 const correct = "Your guess was correct, You Win!"
+
+document.getElementById('submit-button').onclick = () => {
+  console.log('CLICKED')
+  let guessNumber = document.getElementById('guess-input').value
+  document.getElementById('guess-input').value = ""
+  addGuessToList(guessNumber)
+  return checkGuess(guessNumber)
+}
+
+const checkGuess = (number) => {
+  if(number == randomNumber) {
+    document.getElementById('game-output').innerHTML =
+    correct
+  }
+  else if(number > randomNumber) {
+    document.getElementById('game-output').innerHTML =
+      highGuess
+  } else if(number < randomNumber ) {
+    document.getElementById('game-output').innerHTML =
+      lowGuess
+  }
+}
+
+const addGuessToList = (number) => {
+  let newElem = document.createElement('LI');
+  const numText = document.createTextNode(number)
+  newElem.appendChild(numText)
+  const numList = document.getElementById('guess-list')
+  numList.appendChild(newElem)
+}
 const guess = () => {
   const userInput = document.getElementById('guess-input')
+  console.log(userInput)
   const lineBreak = document.createElement('br');
   const userGuessDiv = document.createElement('div');
   userGuessDiv.setAttribute('id', 'guess-log')
@@ -28,9 +65,10 @@ const guess = () => {
       gameAnswerChild = document.createTextNode(gameAnswer)
       userGuessDiv.appendChild(gameAnswerChild)
   } else if(userInput.value == randomNumber) {
-        userGuessDiv.appendChild(lineBreak)
-        gameAnswer = correct
-        gameAnswerChild = document.createTextNode(gameAnswer)
-        userGuessDiv.appendChild(gameAnswerChild)
+      userGuessDiv.appendChild(lineBreak)
+      gameAnswer = correct
+      gameAnswerChild = document.createTextNode(gameAnswer)
+      userGuessDiv.appendChild(gameAnswerChild)
   }
+
 }
