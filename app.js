6a3785c46f6142980cb0e77c84c34a4fd94e993c
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-17 07:30:05 -0400

    js framed out.

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 console.log("HELLO OSCAR PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+
+randomNumber = Math.floor(Math.random()*100 + 1)
+console.log(randomNumber)
+
+gameSolved = false
+
+const guess = () => {
+  userInput = document.getElementById('guess-input')
+  console.log(userInput.value)
+  if(userInput.value > randomNumber) {
+
+  } else if(userInput.value < randomNumber) {
+
+  } else if(userInput.value == randomNumber) {
+    gameSolved = true;
+  }
+
+  if (gameSolved == true) {
+    //set html on page to reflect the game is over.
+  }
+}
